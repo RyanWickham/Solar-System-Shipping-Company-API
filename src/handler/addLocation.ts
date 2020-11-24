a8ab5486e8535afe089932a14d7d0b99b19f64c2
Ryan Wickham <48148988+RyanWickham@users.noreply.github.com>
2020-11-24 15:22:26 +1100

    Made error message more concise

--- src/handler/addLocation.ts
+++ src/handler/addLocation.ts
@@ -17,39 +17,47 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
     //sends the location off to be be delt with -> returns an a message to be sent to the client
     const result = service.addLocation(locationToAdd);
     return IOHandler.returnSuccess(result);
-  }
+}
 
-  const errorChecking = (locationToAdd: {[key: string]: any}): {statusCode: number, body: string} => {
-        //Make sure each element needed was included in request
-        if(!locationToAdd.id || !locationToAdd.cityName || !locationToAdd.planetName || !locationToAdd.totalAvailableCapacity){
+const errorChecking = (locationToAdd: {[key: string]: any}): {statusCode: number, body: string} => {
+    const missingItemMessage: string = "ERROR request does not include all required paramaters. Try '/location/help' to see requirements";
+    const paramaterHasWrongTypeMessage: string = "ERROR a paramater provided is of wrong type. Try '/location/help' to see requirements";
+
+    //Make sure each element needed was included in request
+    if(!locationToAdd.id || !locationToAdd.cityName || !locationToAdd.planetName || !locationToAdd.totalAvailableCapacity){
         return IOHandler.returnError400({
-            message: "ERROR request does not include all required paramaters. Try '/location/help' to see requirements"
+            message: missingItemMessage
         });
     }
 
     //type gard to ensure that each paramater is of correct type
     if(typeof locationToAdd.id != 'string' || typeof locationToAdd.cityName != 'string' || typeof locationToAdd.planetName != 'string'){
         return IOHandler.returnError400({
-            message: "ERROR request does not include all required paramaters. Try '/location/help' to see requirements"
+            message: paramaterHasWrongTypeMessage
         });
     }
 
     if(typeof locationToAdd.totalAvailableCapacity != 'number'){
         return IOHandler.returnError400({
-            message: "ERROR request does not include all required paramaters. Try '/location/help' to see requirements"
+            message: paramaterHasWrongTypeMessage
         });
     }
 
-    //currentAvailableSpace is optional and need extra checks
-    if(!locationToAdd.currentAvailableSpace){
-        locationToAdd.currentAvailableSpace = 0;
+    //currentAmountOfCapacityUsed is optional and need extra checks
+    if(!locationToAdd.currentAmountOfCapacityUsed){
+        locationToAdd.currentAmountOfCapacityUsed = 0;
 
-    }else if(locationToAdd.currentAvailableSpace > locationToAdd.totalAvailableCapacity){
+    }else if(typeof locationToAdd.currentAmountOfCapacityUsed != 'number'){//check it is a number
+        return IOHandler.returnError400({
+            message: paramaterHasWrongTypeMessage
+        });
+
+    }else if(locationToAdd.currentAmountOfCapacityUsed > locationToAdd.totalAvailableCapacity){//check the capacity limit is correct
         //more items are in location than the capacity is allowed
         return IOHandler.returnError400({
-            message: "ERROR more items are in location than the capacity is allowed -> currentAvailableSpace > totalAvailableCapacity"
+            message: "ERROR more items are in location than the capacity is allowed -> currentAmountOfCapacityUsed > totalAvailableCapacity"
         });
     }
 
     return IOHandler.returnSuccess('');//use as a dummy response to signify no errors
-  }
+}
--- src/service.ts
+++ src/service.ts
@@ -2,7 +2,7 @@ export default {
     addLocation: data => {
         return {
             message: 'Location Added: ID: ' + data.id + ', city name: ' + data.cityName + ', planet name: ' + data.planetName
-                + ", total capacity: " + data.totalAvailableCapacity + ", curent available space: " + data.currentAvailableSpace,
+                + ", total capacity: " + data.totalAvailableCapacity + ", curent amount of space used: " + data.currentAmountOfCapacityUsed,
         }
     }
 }
